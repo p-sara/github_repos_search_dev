3ed79921dda7de82ba18244a9f64c1a8db9dbbb5
Sara Pstrowska <pstrowska.sara@gmail.com>
2020-09-19 14:09:01 +0200

    changed name of a repo to correct one in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:p-sara/github-repo-searcher.git master:gh-pages
+git push -f git@github.com:p-sara/github_repos_search_dev.git master:gh-pages
 
 cd -
